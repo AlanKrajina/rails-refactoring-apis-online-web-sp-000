df4e5b18b79c5bf58a0c9470aad59a897c3d2148
Alan Krajina <alankrajina88@gmail.com>
2019-09-28 13:29:53 +0000

    Automatically backed up by Learn

--- app/service/github_service.rb
+++ app/service/github_service.rb
@@ -8,7 +8,7 @@ class GithubService
 
   def authenticate!(client_id, client_secret, code)
     resp = Faraday.post("https://github.com/login/oauth/access_token") do |req|
-      req.body = { 'client_id': ENV["GITHUB_CLIENT"], 'client_secret': ENV["GITHUB_SECRET"], 'code': params[:code] }
+      req.body = { 'client_id': client_id, 'client_secret': client_secret, 'code': code }
       req.headers['Accept'] = 'application/json'
     end
 
